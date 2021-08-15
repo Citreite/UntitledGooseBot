4cbd7b6489d34248c6dde8134a6c6a54980d544a
Peter Yao <novodoodle@gmail.com>
2021-08-15 04:25:30 +0000

    Initital commit - Added remind command and set up the base files.

---
+++ commands/remind.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const wait = require('util').promisify(setTimeout);
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('remind')
+        .setDescription('The goose will remind you to do something after some time.')
+        .addIntegerOption(option =>
+            option.setName('time')
+                .setDescription('The time you want the goose to remind you later (in minutes)')
+                .setRequired(true),
+        )
+        .addStringOption(option =>
+            option.setName('stuff')
+                .setDescription('What you want the goose to remind you to do (e.g. homework)')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const time = interaction.options.getInteger('time');
+        const stuff = interaction.options.getString('stuff');
+        
+        if (time >= 15){
+        
+        // If the timeframe is more than 15 minutes
+        await interaction.reply({ content: `<@${interaction.user.id}> I cannot set a timer for more than 15 minutes; I am too lazy to do that.`, ephemeral: true });
+
+        } else {
+        
+        // Execute if it is less
+        await interaction.reply({ content: `<@${interaction.user.id}> I have set a timer for ${time} minutes to remind you about ${stuff}.`, ephemeral: true });
+        
+        await wait(time*60000); // The time period in minutes
+        await interaction.reply(`<@${interaction.user.id}> HONK! It's been ${time} minutes; don't forget to do ${stuff}.`);
+        
+        }
+
+    },
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+// Required imports
+const fs = require('fs');
+const dotenv = require('dotenv');
+const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const express = require('express');
+
+// Dotenv config
+dotenv.config();
+
+// Client setup
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+
+// Command handling
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// Registering slash commands
+const commands = [];
+
+// Place your client and guild ids here
+const clientId = '876091817802739722';
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+            Routes.applicationCommands(clientId), { body: commands },
+        );
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+// Event handling
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const { commandName } = interaction;
+	if (!client.commands.has(commandName)) return;
+
+	try {
+		await client.commands.get(commandName).execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Log into the client using the environment variable
+client.login(process.env.TOKEN);
+
+// Website for uptimerobot to ping
+require('http').createServer((req, res) => res.end('Bot is alive!')).listen(3000);
